71ab7b572c2fefc55f48a3adca04defd7a7e31bd
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2023-01-27 14:38:09 -0600

    2 files that I have created on stream

---
+++ arrayTest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <string>
+int main()
+{
+    string favoriteGames[3]=["Stardew Valley", "Mario     Sunshine","Castlevania"];
+for(int i=0; i<3; i++)
+{
+    std::cout<< favoriteGames[i]<<std::endl;
+}
+return 0;
+}
+
---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+// function code goes here
+cout<<"Hello World\n";
+cout<< "This is end of line"<<endl;
+cout<<"test finished\n";
+cout<< "What day is it today?\n";
+string day;
+cin>> day;
+cout<<"You answered with "<<day <<endl;
+
+return 0;
+}
